0f81e7e6a6010b86cc24b37b30751f78f5802852
Ziggy <zachariahjweaver@gmail.com>
2022-07-07 22:56:17 -0400

    Added more sloppy psuedo code on table builders. Need to finish setting up PostgreSQL DB before I can really test functionality of SQL code

--- DatabaseAPI/TweetNumberToDataTableBuilder.py
+++ DatabaseAPI/TweetNumberToDataTableBuilder.py
@@ -1,12 +1,9 @@
 # this loop collects new tweets and stores the data points Number to data table
-# psudo code placeholder, need to find how to stream tweets from an twitter account
-
-source = tweet.source
-created_at = tweet.created_at
-attachments = tweet.attachments
-text = tweet.text
-public_metrics = tweet.public_metrics
-id = tweet.id
+# psuedo code placeholder, need to find how to stream tweets from an twitter account
 
 WordsToTweetNumbers(tweet.text)
+
+INSERT INTO Tweets(number, source, created_at, attachments, text, public_metrics, id)
+VALUES (tweet.source, tweet.created_at, tweet.attachments, tweet.text, tweet.public_metrics, tweet.id);
+
 
--- DatabaseAPI/WordToTweetNumbersTableBuilder.py
+++ DatabaseAPI/WordToTweetNumbersTableBuilder.py
@@ -1,5 +1,7 @@
 for words in tweettext: #messy psuedo code for building word to tweet number table
     if db.contains word:
         db.addNumber(word, number)
+        newNumbers =numbers + SELECT numbers FROM Words
+        UPDATE Words SET number = newNumbers, WHERE CustomerID = word
     else:
-        db.addRow(word, number)
+        INSERT INTO Words(word, number)
