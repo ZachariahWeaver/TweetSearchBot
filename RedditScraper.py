d16d3d5c3ef6219655e34478595754791512cf1f
Ziggy <zachariahjweaver@gmail.com>
2022-07-14 17:39:49 -0400

    Fixed reddit reply to insert variables pulled from DB.

--- RedditScraper.py
+++ RedditScraper.py
@@ -2,13 +2,13 @@ import configparser
 import string
 import psycopg2
 import praw
-import random
+from datetime import datetime
 
 config = configparser.ConfigParser()
 config.read('CONFIG.INI')
 
 reddit = praw.Reddit(
-    client_id=config['reddit']['clientid'],  # dummy data to keep keys private
+    client_id=config['reddit']['clientid'],
     client_secret=config['reddit']['clientsecret'],
     password=config['reddit']['password'],
     user_agent="Console:SaleNotifierBot:v1.0 (by u/z_weaver))",
@@ -24,12 +24,33 @@ conn = psycopg2.connect(
 cursor = conn.cursor()
 
 
-def getLink(tweetnumber):
-    account = 'fake'
-    return ("https://twitter.com/user/status/%s") % (tweetnumber,)
+def getlink(tweetnumber):
+    return "https://twitter.com/user/status/%s" % (tweetnumber,)
+
+def getdate(iso_string):
+    date_time = datetime.strptime(iso_string, "%Y-%m-%dT%H:%M:%S.%fZ")
+    return "%s:%s %s· %s %s, %s · " % (date_time.strftime('%I'), date_time.strftime('%M'), date_time.strftime('%p'), date_time.strftime('%b'), date_time.strftime('%d'), date_time.strftime('%Y'))
+
+def gettext(tweettext):
+    formattedtweettext = "    "
+    index = 0
+    tweettext = tweettext.replace("\n", " ")
+    print(tweettext)
+    tweettext = list(tweettext.split(" "))
+    print(tweettext)
+    while index < len(tweettext):
+        formattedtweettext = formattedtweettext + tweettext[index] + " "
+        index = index + 1
+        if index % 10 == 0:
+            formattedtweettext = formattedtweettext + "\n    "
+
+    if index % 10 != 0:
+        formattedtweettext = formattedtweettext + "\n    "
+
+    return formattedtweettext
 
 
-for comment in reddit.subreddit("test").stream.comments(skip_existing=True):
+for comment in reddit.subreddit("testingground4bots").stream.comments(skip_existing=True):
     print("New comment detected! \n")  # console statement to show bot is detecting new comments
     reply = ""  # Start with an empty string which will be added to making the final reply
     fail = False
@@ -37,9 +58,10 @@ for comment in reddit.subreddit("test").stream.comments(skip_existing=True):
             comment.body) < 286:  # check for request marker and refuse requests that are too long
         print("Comment requests bot utility!\n \n")
         reply_opening = "The words you want to search are: "
-        reply_ending = "_____\n^^(Hello, I'm TweetSearchBpt, I find tweets for you" \
-                       "and reply with a formatted version here on reddit)[^(For more info, requests or complaints go here)](" \
-                       "https://github.com/ZachariahWeaver/TweetSearchBot) "
+        reply_ending = '_____ \n^(Hello, I\'m TweetSearchBot, I find tweets for you' \
+                       'and reply with a formatted version ^(For more info, requests or complaints go ' \
+                       'here)](' \
+                       'https://github.com/ZachariahWeaver/TweetSearchBot) '
         s = comment.body[5:]  # take the comment without the bot tag  and leading space(!SNB )
         s = s.translate(str.maketrans('', '', string.punctuation))  # remove punctuation
         s = s.lower()
@@ -48,52 +70,53 @@ for comment in reddit.subreddit("test").stream.comments(skip_existing=True):
         if s:
             postgres_select_query = """SELECT tweetnumber FROM tweet WHERE word = '%s'""" % (s[0],)
             cursor.execute(postgres_select_query)
-            if cursor.fetchone() is not None:
-                a = cursor.fetchone()[0]
+            a = cursor.fetchone()[0]
+            print(a)
+            for word in s:
+                postgres_select_query = """SELECT tweetnumber FROM tweet WHERE word = '%s'""" % (word,)
+                cursor.execute(postgres_select_query)
+                tweetnumber_records = cursor.fetchone()[0]
+                a = list(set(a) & set(tweetnumber_records))
                 print(a)
-                for word in s:
-                    postgres_select_query = """SELECT tweetnumber FROM tweet WHERE word = '%s'""" % (word,)
-                    cursor.execute(postgres_select_query)
-                    tweetnumber_records = cursor.fetchone()[0]
-                    a = list(set(a) & set(tweetnumber_records))
-                    print(a)
-                if a:
-                    for id, tweet in enumerate(a):  # add all tweets that match to comment reply
-                        print(tweet)
-                        reply = "{0}{1}".format(reply, (
-                                "    ←       Tweet\n"
-                                "    ⬛⬛⬛ Ryan Cohen 🅥                                     \n"
-                                "    ⬛🧔⬛ @ryancohen\n"
-                                "    ⬜⬜⬜\n"
-                                "    \n"
-                                "    I’m sick of seeing failed executives make millions in risk\n"  # replace with variable that is a formated vers of text
-                                "    free compensation while shareholders are left holding the\n"  #
-                                "    bag\n"  #
-                                "\n"
-                                "    10:56 AM · Jun 29, 2022 · Twitter for iPhone\n"  # variable that has date and source
-                                "    ――――――――――――――――――――――――――――――――――――――――――――――――――――――――――\n"
-                                "\n"
-                                "    4,323 Retweets      302 Quote Tweets      25.9K Likes\n"  # variable that has public metrics
-                                "    ――――――――――――――――――――――――――――――――――――――――――――――――――――――――――\n"
-                                "           🗨️             🔄             ❤️             🔗\n"
-                                "    ――――――――――――――――――――――――――――――――――――――――――――――――――――――――――\n"
-                                "[Tweet " + str(id + 1) + "]" + "(" + getLink(tweet) + ") \n\n"))
-                else:
-                    fail = True
-                    print("fail 1")
+            if a:
+                for id, tweet in enumerate(a):  # add all tweets that match to comment reply
+                    print(tweet)
+                    postgres_tweetid_select_query = """SELECT * FROM tweetid WHERE tweetnumber = %s""" % (tweet,)
+                    cursor.execute(postgres_tweetid_select_query)
+                    tweetinfo = cursor.fetchone()
+                    print(tweetinfo)
+                    reply = "{0}{1}".format(reply, (
+                            '    ←       Tweet\n'
+                            '    ⬛⬛⬛ Ryan Cohen 🅥                                     \n'
+                            '    ⬛🧔⬛ @ryancohen\n'
+                            '    ⬜⬜⬜\n'
+                            '    \n'
+                            + gettext(tweetinfo[1]) +
+                            '\n'
+                            '    %s%s\n' % (getdate(tweetinfo[2]), tweetinfo[4]) +  # variable that has date and source
+                            '    ――――――――――――――――――――――――――――――――――――――――――――――――――――――――――\n'
+                            '\n'
+                            '    %s Retweets      %s Quote Tweets      %s Likes\n' % (
+                            tweetinfo[7], tweetinfo[6], tweetinfo[5]) +
+                            '    ――――――――――――――――――――――――――――――――――――――――――――――――――――――――――\n'
+                            '           🗨️             🔄             ❤️             🔗\n'
+                            '    ――――――――――――――――――――――――――――――――――――――――――――――――――――――――――\n'
+                            '[Tweet ' + str(id + 1) + "]" + "(" + getlink(tweet) + ") \n\n"))
             else:
                 fail = True
+                print("fail 1")
         else:
             fail = True
             print("fail 2")
 
         word_list = str(s)
         word_list = word_list.replace("\'", "")
-        if (fail):
+        if fail:
             print("Unfortunately the words you requested were not found all at once in any one tweet! Try again using "
                   "fewer terms or checking your spelling.")
             comment.reply(body=("Unfortunately the words you requested were not found all at once in any one tweet! "
-                                "Try again using fewer terms or checking your spelling." + reply_ending))
+                                "Try again using fewer terms or checking your spelling. " + reply_ending))
+
         else:
             print(reply_opening + "\n" + word_list + "\n" + reply)
 
--- TwitterScraper.py
+++ TwitterScraper.py
@@ -13,13 +13,16 @@ access_token = config['twitter']['accesstoken']
 access_token_secret = config['twitter']['accesstokensecret']
 bearer_token = config['twitter']['bearertoken']
 
+lasttweet = None
 client = tweepy.Client(bearer_token=bearer_token, consumer_key=consumer_key, consumer_secret=consumer_secret,
                        access_token=access_token, access_token_secret=access_token_secret, return_type=dict,
                        wait_on_rate_limit=False)
-dict = client.get_users_tweets(id=1544738842611154946, max_results=100, user_auth=False)
+dict = client.get_users_tweets(id=1544738842611154946, max_results=100, since_id = lasttweet, user_auth=False, tweet_fields=['created_at','source,id','text','public_metrics'])
 print(dict)
 
 temp = dict.get('data')
+meta = dict.get('meta')
+lasttweet = meta.get('oldest_id')
 conn = psycopg2.connect(
     host=config['postgresql']['host'],
     database=config['postgresql']['database'],
@@ -29,6 +32,10 @@ conn = psycopg2.connect(
 cursor = conn.cursor()
 
 for d in temp:
+    metrics = d.get('public_metrics')
+    postgreSQL_insert_Query = "INSERT INTO tweetid (tweetnumber, tweettext, tweetdate, tweetimage, tweetsource, tweetlikecount, tweetquotecount, tweetretweetcount) VALUES(%s, '%s', '%s', '%s', '%s', '%s', '%s', '%s')" % (d.get('id'),d.get('text'),d.get('created_at'),'none',d.get('source'),metrics.get('like_count'),metrics.get('quote_count'),metrics.get('retweet_count'))
+    cursor.execute(postgreSQL_insert_Query)
+
     numbers = d.get('id')
     text = d.get('text').lower()
     text = text.translate(str.maketrans('', '', string.punctuation))
